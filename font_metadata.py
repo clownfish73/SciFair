03f55534b96edb5c9c295657ba7eef6f00875497
plant-ai <149395424+plant-ai@users.noreply.github.com>
2025-02-12 21:01:53 +0000

    Removed API key

--- font_metadata.py
+++ font_metadata.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "AIzaSyB6S-5z2QJEOaAEuk94OmfitvKZYdJVN-4"
+API_KEY = ""
 url = f"https://www.googleapis.com/webfonts/v1/webfonts?key={API_KEY}"
 response = requests.get(url)
 fonts = response.json().get('items', [])
